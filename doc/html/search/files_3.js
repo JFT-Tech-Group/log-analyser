5093b580f99645358f1789a6b38936b4de368480
JFT-Tech-Group <al.andrianarimanana1@gmail.com>
2025-08-29 12:44:27 +0300

    Documentation

---
+++ doc/html/search/files_3.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['stats_2ec_0',['stats.c',['../stats_8c.html',1,'']]]
+];
---
+++ doc/html/search/searchdata.js
@@ -0,0 +1,18 @@
+var indexSectionsWithContent =
+{
+  0: "flmsu",
+  1: "flmsu"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "files"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Files"
+};
+
